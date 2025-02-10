0351f015929a414a3957327c8e0ae4a9b654f513
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-10 12:19:07 +0900

    🐛 Fixed: Service Worker type issues
    Fixed: Service Worker type issues

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0nq3dpvesb"
+    "revision": "0.lph4rbsab8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
